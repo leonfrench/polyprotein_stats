3b2ff96e7445da29e56b70b6b2ab0ae70621a77d
leonfrench <leonfrench@gmail.com>
2024-11-04 20:56:39 -0500

    Updates for new python version

--- protein_embeddings_app.py
+++ protein_embeddings_app.py
@@ -18,13 +18,11 @@ from statsmodels.stats import multitest
 import bokeh.io
 import bokeh.plotting
 
-from matplotlib.backends.backend_agg import RendererAgg
-_lock = RendererAgg.lock
 
 embedding_file_path_processed = os.path.join(os.path.dirname(__file__), 'data', 'CZI_image_spectra')
 
 #cache the file loading to speed things up
-@st.cache
+@st.cache_data
 def get_file_with_cache(filename):
     df = pd.read_csv(os.path.join(embedding_file_path_processed, filename))
     return df
@@ -156,13 +154,13 @@ aa_AUC_df_square = (aa_AUC_df.pivot(index=['residue B'],columns='residue A', val
     
 
 sns.set_theme()
-with _lock:
-  fig = plt.figure(figsize=(10, 7))
-  aa_AUC_df_square = aa_AUC_df_square.fillna(0.5)
-  ax = sns.heatmap(aa_AUC_df_square, center = 0.5, cmap = 'vlag')
 
-  st.pyplot(fig, clear_figure = True)
-  plt.close("all")
+fig = plt.figure(figsize=(10, 7))
+aa_AUC_df_square = aa_AUC_df_square.fillna(0.5)
+ax = sns.heatmap(aa_AUC_df_square, center = 0.5, cmap = 'vlag')
+
+st.pyplot(fig, clear_figure = True)
+plt.close("all")
 
 #tag on length AUC value, could just be printed
 auc_for_length = roc_auc_score(proportions['classification_target'], proportions['length'])
